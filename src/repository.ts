7cb0ef2bac6de83ce7fc2ddc460c2195a3342bd4
gustavovalle <gustavo.vallerp@hotmail.com>
2024-07-27 21:52:43 +0200

    build: add npm build

--- src/repository.ts
+++ src/repository.ts
@@ -4,14 +4,19 @@ export abstract class Repository<Entity> {
   abstract knex: any;
   abstract entity: { new(): Entity };
 
+  private toEntity(data: any): Entity {
+    // @ts-ignore
+    return Object.assign(new this.entity(), data);
+  }
+
   async findById(id: string): Promise<Entity | null> {
     const result = await this.knex(this.table).where({ id, deleted_at: null }).first();
-    return result ? Object.assign(new this.entity(), result) : null;
+    return result ? this.toEntity(result) : null;
   }
 
   async findAll(): Promise<Entity[]> {
     const results = await this.knex(this.table).where({ deleted_at: null });
-    return results.map((result: any) => Object.assign(new this.entity(), result));
+    return results.map((result: any) => this.toEntity(result));
   }
 
   async insert(entity: Entity): Promise<Entity> {
